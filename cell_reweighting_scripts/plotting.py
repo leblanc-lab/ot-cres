c02c4990c63f04b1bcbb86fe27a60743e3cb5862
laurenhay <lauren_hay@brown.edu>
2026-04-21 15:06:44 -0400

    Make plots match Rishabh's more -- fix bug in njets

--- cell_reweighting_scripts/plotting.py
+++ cell_reweighting_scripts/plotting.py
@@ -4,8 +4,16 @@ import matplotlib.cm as cm
 import hist
 import numpy as np
 import mplhep as hep
+import re
 hep.style.ROOT
-cmap  = cm.tab10.colors 
+cmap  = ["magenta", "red", "blue", "gold", "lime"]
+
+def clean_filename(s):
+    s = re.sub(r'\$*?\$', '', s)        # remove LaTeX math
+    s = re.sub(r'\.', 'p', s)  ###replace points w/ p
+    s = re.sub(r'[^A-Za-z0-9]+', '_', s) # non-alphanumeric → _
+    s = re.sub(r'_+', '_', s)            # collapse _
+    return s.strip('_')
 
 def get_ratio_unc(num, denom):
     val_num = num.values()
@@ -18,11 +26,11 @@ def get_ratio_unc(num, denom):
     ratio_var = (ratio**2)*rel_var
     return ratio, ratio_var
 
-def plot_same_rw_all(obs, df, weights_orig, numbins, xmin, xmax, obs_str = "", sel=None, ymin=1e-4, ymax=1e0, logy=True, title="TTbar",raxlim=[0.85, 1.15]):
+def plot_same_rw_all(obs, df, weights_orig, numbins, xmin, xmax, obs_str = "", sel=None, ymin=1e-4, ymax=1e0, logy=True, title="TTbar", channel="zjets_100k",raxlim=[0.85, 1.15]):
     if sel is None:
         sel  = np.ones_like(weights_orig, dtype=bool)
     sel_weights = weights_orig[sel]
-    cmap  = cm.tab10.colors 
+    cmap  = cm.Dark2.colors 
     fig, (ax, rax) = plt.subplots(nrows=2,
                             ncols=1,
                             figsize=(8,6),
@@ -36,6 +44,7 @@ def plot_same_rw_all(obs, df, weights_orig, numbins, xmin, xmax, obs_str = "", s
         )
     h_orig.fill(obs, weight = sel_weights)
     h_orig = h_orig/h_orig.sum(flow=False).value
+    hep.histplot(h_orig, ax=ax, label = "Original", color='black')
     for i, R in enumerate(df["radius"].values):
         weights = np.array(df.loc[df["radius"]==R, "weights"].squeeze())[sel]
         frac = df.loc[df["radius"]==R, "fraction"].iloc[0]
@@ -53,7 +62,6 @@ def plot_same_rw_all(obs, df, weights_orig, numbins, xmin, xmax, obs_str = "", s
         hep.histplot(ratio, bins=bin_edges, ax=rax, histtype='step', yerr = np.sqrt(ratio_unc))
         hep.histplot(h, ax=ax, label = f"{round(frac*100)}% RW (R={R} GeV)")
     hep.histplot(np.ones_like(ratio), bins=bin_edges, ax=rax, yerr = np.sqrt(ratio_unc1), color='black')
-    hep.histplot(h_orig, ax=ax, label = "Original", color='black')
     rax.set_xlabel(rf"${obs_str}$")
     rax.set_ylabel("Reweight/Orig")
     ax.set_title(title)
@@ -66,10 +74,10 @@ def plot_same_rw_all(obs, df, weights_orig, numbins, xmin, xmax, obs_str = "", s
     ax.set_ylim(ymin, ymax)
     ax.legend()
     plt.subplots_adjust(hspace=0.05)
-    plt.savefig(f"../plots/ttbar_100k/comp_{title}_{obs_str[0]}.png")
+    plt.savefig(f"../plots/{channel}/{title}_{obs_str}.png")
     plt.show()
 
-def plot_diff_rw(obs, dfs, strings, fracs, weights_orig, xmin, xmax, nbins, ymin=1e-4, ymax=1e0, obs_str = "", sel=None, title="", raxlim=[0.85, 1.15]):
+def plot_diff_rw(obs, dfs, strings, fracs, weights_orig, xmin, xmax, nbins, ymin=1e-5, ymax=1e0, obs_str = "", sel=None, title="", raxlim=[0.85, 1.15], channel = "zjets_100k"):
     if sel is None:
         sel  = np.ones_like(weights_orig, dtype=bool)
     fig, (ax, rax) = plt.subplots(nrows=2,
@@ -82,6 +90,7 @@ def plot_diff_rw(obs, dfs, strings, fracs, weights_orig, xmin, xmax, nbins, ymin
         storage=hist.storage.Weight(), )
     h_orig.fill(obs, weight = weights_orig[sel])
     h_orig = h_orig/h_orig.sum(flow=False).value
+    hep.histplot(h_orig, ax=ax, label = "Original", color='black')
     for i, df in enumerate(dfs):
         weights = np.array(df.loc[df["fraction"]==fracs[i], "weights"].squeeze())[sel]
         h = hist.Hist(
@@ -98,8 +107,6 @@ def plot_diff_rw(obs, dfs, strings, fracs, weights_orig, xmin, xmax, nbins, ymin
         hep.histplot(np.ones_like(ratio), bins=bin_edges, ax=rax, yerr = np.sqrt(ratio_unc1), color='black')
         hep.histplot(ratio, bins=bin_edges, ax=rax,  color=cmap[i], histtype='errorbar', yerr = np.sqrt(ratio_unc), )
         hep.histplot(h, ax=ax, label = f"{round(fracs[i]*100)}% RW {strings[i]}", color=cmap[i], histtype='errorbar')
-        
-    hep.histplot(h_orig, ax=ax, label = "Original", color='black')
     rax.set_xlabel(rf"${obs_str}$")
     rax.set_ylabel("Reweight/Orig")
     ax.set_title(title)
@@ -110,5 +117,5 @@ def plot_diff_rw(obs, dfs, strings, fracs, weights_orig, xmin, xmax, nbins, ymin
     ax.legend()
     ax.set_yscale('log')
     plt.subplots_adjust(hspace=0.05)
-    plt.savefig(f"../plots/ttbar_100k/comp_frac_{round(fracs[0], 0)}_{obs_str[0]}.png")
-    plt.show()
+    filename = clean_filename(f"{title}_{obs_str}_{fracs[0]}")
+    plt.savefig(f"../plots/{channel}/{filename}")
